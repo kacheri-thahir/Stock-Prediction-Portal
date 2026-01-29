bad731fa145445d4d16ac4ba169d0329ace9a542
Kacheri Thahir <thahirkacheri2@gmail.com>
2026-01-29 12:35:53 +0530

    Register Backend

---
+++ Backend-drf/accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True,min_length=8,style={'input_type':'password'},validators=[validate_password]) #one should not see the password by get request so we assign write only here, style={'input_type':'password'} gives *** as we type password in password field,min_length=8 or any number like 9, 10 or anything handles password minimum length(must be 8 chars),validators=[validate_password] checks like contain atleast 1 Uppercase,special chars like that
+    class Meta:
+        model=User
+        fields=['username','email','password']
+
+    def create(self, validated_data):
+        # User.objects.create = saves password in plain text
+        # User.objects.create_user = automatically hashes the password that why we used create_user
+        user=User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'],
+        )
+        # user=User.objects.create_user(**validated_data) we can also use the method too but if have any personal info need to use upper method 
+        return user
